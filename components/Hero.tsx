55f82314e59fbe6e7676a61b5a7f09bf79a41507
Reginald Nwachukwu <nwachukwureg05@gmail.com>
2025-11-07 11:07:27 +0100

    correction

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
     
   return (
     <section className='p-8 min-h-screen w-full text-white grid grid-cols-1 justify-center gap-4 bg-[url(/chem7.jpg)] brightness-70 object-fill bg-cover'>
-        <div className='grid gap-2 md:max-w-[60vw] h-[65vh] mx-auto'>
+        <div className='grid gap-2 md:max-w-[70vw] h-[65vh] mx-auto'>
             <h2 className='text-3xl md:text-7xl font-extrabold align-bottom pt-14'>Cabolet Coat Chemicals.</h2>
             <p  className='text-xl md:text-3xl font-normal'>
                 Cabolet Coat Chemicals (CCC) is one of the leading distributors of
